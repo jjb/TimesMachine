2ed552c0f083836ebc2982648951fc751635e3fc
John Joseph Bachir <j@jjb.cc>
2011-12-03 20:19:45 -0500

    make api call for terms and date

--- lib/demand_feedia.rb
+++ lib/demand_feedia.rb
@@ -2,16 +2,20 @@ require 'grape'
 require 'http'
 require 'json'
 
-
 class DemandFeedia < Grape::API
-  resource :messages do
-    get :hello do
-      HTTP.get 'http://api.nytimes.com/svc/search/v1/article?format=json&query=steven+spielberg+fee%3A%22Y%22&begin_date=19810101&end_date=20120101&rank=newest&api-key=3761c354783251ab639777f67fdcae98:5:65301959'
+  resource :articles do
+    get '/about/:terms/around/:year' do
+      year = params[:year].to_i
+      begin_date = "#{year - 1}0701"
+      end_date   = "#{year + 1}0631"
+      HTTP.get 'http://api.nytimes.com/svc/search/v1/article?format=json' +
+        '&query=' + params[:terms] +
+        '+fee%3A%22Y%22&' +
+        "begin_date=#{begin_date}&" +
+        "end_date=#{end_date}&" +
+        'rank=closest&' +
+        'api-key=3761c354783251ab639777f67fdcae98:5:65301959'
     end
-
-    # get '/hello/:entity' do
-    #   "Hello, #{params[:entity]}"
-    # end
   end
   
 end
